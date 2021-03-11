164d584f8d6b4730e46b0b266b1187b05c25bafc
Sz77 <63186233+Sz77@users.noreply.github.com>
2021-03-11 10:23:36 +0200

    Q2

--- week 16/stefanHW_16Q2/prog.c
+++ week 16/stefanHW_16Q2/prog.c
@@ -1,28 +1,50 @@
 /*********************************
-* Class: MAGSHIMIM C2             *
-* Week 1                            *
+* Class: MAGSHIMIM C2			 *
+* Week 1           				 *
+* HW Question - Thanks Program 	 *
 **********************************/
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
+void printMessage(char str[]);
 
 int main(void)
 {
-    int bobDylan = 50;
+    //##########################################################################################################
+	//#comp error                                                                                               #
+	//#strncat needs 2 strings/arrays of chars so you needs to be an array with the length of 1                #
+	//#run time error                                                                                           #               
+	//#the array str was to short it needed atlist 7 for the size                                               #
+	//#The conventions error                                                                                    #
+	//#the comments should be in English                                                                       #     
+    //##########################################################################################################
+	char str[7] = "THANK ";
+	char you[1] = "u";
 
-    int ummKulthum = 100;
-    int janisJoplin = 10;
-    int johnnyCash = 90;
-    int duduTasa = 12;
-    // bobDylan | ummKulthum  |  janisJoplin  | johnnyCash
-    // --------   -----------   -------------   ---------------
-    johnnyCash /= 3;                                            //    50    |     100     |       10      |        30
-    bobDylan = ummKulthum + janisJoplin++ * 3;                  //    50    |     100     |       10      |        30
-    bobDylan += --johnnyCash + janisJoplin + ummKulthum;        //   130    |     100     |       11      |        30
+	strncat(str, you, 1); // do not fix this line or the next one
+	printMessage(str);
+	return 0;
+}
 
-    printf("How many roads must a man walk down before you can call him a man?\n");
-    printf("The answer my friend, is %d\n", bobDylan);
-    getchar();
-    return 0;
+/*
+printing the masseg
+*/
+void printMessage(char str[])
+{
+	printf("Hello Sigal! I just wanted to say that studying with you...\n\n");
+	printf("Really made me understand how much I love\n");
+	printf(" __  __                 _     _           _           \n");
+	printf("|  \\/  |               | |   (_)         (_)          \n");
+	printf("| \\  / | __ _  __ _ ___| |__  _ _ __ ___  _ _ __ ___  \n");
+	printf("| |\\/| |/ _` |/ _` / __| '_ \\| | '_ ` _ \\| | '_ ` _ \\ \n");
+	printf("| |  | | (_|s | (_| \\__ \\ | | | | | | | | | | | | | | |\n");
+	printf("|_|  |_|\\__,_|\\__, |___/_| |_|_|_| |_| |_|_|_| |_| |_|\n");
+	printf("              __/ |                                  \n");
+	printf("              |___/                                  \n\n");
+
+	printf("Without C you would be just Gal :D\n");
+	printf("See you in class!\n");
+	printf("and %s !!", str);
+
 }
