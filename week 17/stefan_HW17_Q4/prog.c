981b667127033763bb3ed27ce1c0a0e6fc1cdd1d
Sz77 <63186233+Sz77@users.noreply.github.com>
2021-03-23 09:12:02 +0200

    week 17 Q 4

--- week 17/stefan_HW17_Q4/prog.c
+++ week 17/stefan_HW17_Q4/prog.c
@@ -7,11 +7,52 @@
 
 #include <stdio.h>
 
+void swap(float* num1, float* num2);
+void sort(float* num1, float* num2 ,float* num3);
 int main(void)
 {
-
-	//Write your code here...
+	float num1 ,num2 ,num3;
+	//input
+	printf("enter three nums");
+	scanf("%f %f %f", &num1, &num2, &num3);
+	getchar();//buffer cleaner
+	//calling the sort function
+	sort(&num1, &num2, &num3);
+	//output 
+	printf("the sorted numbers first %f , second %f third %f", num1, num2, num3);
 
 	getchar();
 	return 0;
+}
+/*
+this function sorts the numbers and the biggest in num 3
+input: the number to be sorted
+output: none
+*/
+void sort(float* num1, float* num2, float* num3)
+{
+	if (*num1 > *num2)
+	{
+		swap(num1, num2);
+	}
+    if (*num1 > *num3)
+    {
+        swap(num1 , num3);
+    }
+	if (*num2 > *num3)
+	{
+		swap(num2, num3);
+	}	
+}
+
+/*
+this func swaps between two floats using the pointers
+input: the pointers
+output: none
+*/
+void swap(float* num1, float* num2)
+{
+	float temp = *num1;
+	*num1 = *num2;
+	*num2 = temp; 
 }
