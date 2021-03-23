309376e9eac39263fe4bc70feb893c0d10e6bac4
Sz77 <63186233+Sz77@users.noreply.github.com>
2021-03-23 20:13:17 +0200

    week 17

--- week 17/stefan_HW17_Q8/prog.c
+++ week 17/stefan_HW17_Q8/prog.c
@@ -6,12 +6,38 @@
 **********************************/
 
 #include <stdio.h>
-
+void inputArray(int* arr);
+#define SIZE 10
 int main(void)
 {
+	int i = 0;
+	int arr[SIZE] = { 0 };
+	int* p = arr;
+	inputArray(arr);
 
-	//Write your code here...
+	for (i = 0; i < SIZE; i++)
+	{
+		printf("%d", *(p + i));
+	}
+
 
 	getchar();
 	return 0;
+}
+
+
+/*
+this func is the input function
+input: the array
+output: none
+
+*/
+void inputArray(int* arr)
+{
+	int i = 0;
+	int* p = arr;
+	for (i = 0; i < SIZE; i++)
+	{
+		scanf("%d", (p + i));
+	}
 }
